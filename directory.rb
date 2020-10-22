f55b177690fc757a9dbc6b0c8581611afd375b20
Georgie Horth <georgiehorth@gmail.com>
2020-10-22 15:27:54 +0100

    added interactive menu

--- directory.rb
+++ directory.rb
@@ -24,11 +24,11 @@ def input_students
     if students.count == 1
       puts "Now we have #{students.count} student"
     else
-      puts "Now we have #{students.count} student"
+      puts "Now we have #{students.count} students"
     end
     # this is how we get more than one student inputted
     name = gets.chomp
-    puts "What cohort are they in?"
+    #puts "What cohort are they in?"
    cohort = gets.chomp.to_sym.capitalize
   end
   end
@@ -54,9 +54,37 @@ def print_footer(students)
     puts "Overall, we have #{students.count} great students"
   end
 end
-#nothing happens until we call the methods
-students = input_students
 
-print_header
-print(students)
-print_footer(students)
+def interactive_menu
+  students = []
+  loop do
+  # 1. print the menu and ask the user what to do
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9. Exit" #9 because we'll add more items
+  # 2. read the input and save it into a variable
+    selection = gets.chomp
+  # 3. do what the user has asked
+    case selection
+    when "1"
+      students = input_students
+    when "2"
+      print_header
+      print(students)
+      print_footer(students)
+    when "9"
+      exit
+    else
+      puts "I don't know what you meant, try again"
+    end
+  end
+end
+
+
+
+#nothing happens until we call the methods
+#students = input_students
+#print_header
+#print(students)
+#print_footer(students)
+interactive_menu
