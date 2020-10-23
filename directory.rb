08b03dd837e7597dfebc10d09c5d6a623be454ee
Georgie Horth <georgiehorth@gmail.com>
2020-10-23 18:34:53 +0100

    Load students added

--- directory.rb
+++ directory.rb
@@ -3,7 +3,7 @@ def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
   # get the first name
-  name = gets.chomp
+  name = STDIN.gets.chomp
   while !name.empty? do
     @students << {name: name, cohort: :november}
     if @students.count == 1
@@ -12,7 +12,7 @@ def input_students
       puts "Now we have #{@students.count} students"
     end
     # this is how we get more than one student inputted
-    name = gets.chomp
+    name = STDIN.gets.chomp
   end
   #return the array of students
   @students
@@ -21,7 +21,7 @@ end
 def interactive_menu
   loop do
   print_menu
-  process(gets.chomp)
+  process(STDIN.gets.chomp)
   end
 end
 
@@ -44,11 +44,15 @@ def process(selection)
   when "1"
     input_students
   when "2"
-    show_students
+    if @students.count > 0
+      show_students
+    else
+      puts "Oops! There are no students. Please input them"
+    end
   when "3"
     save_students
   when "4"
-    load_students
+    load_students #will load from students.csv by default
   when "9"
     exit
   else
@@ -88,12 +92,26 @@ def save_students
   file.close
 end
 
-def load_students
-  file = File.open("students.csv", "r")
+def load_students(filename = "students.csv")
+  file = File.open(filename, "r")
   file.readlines.each do |line|
     name, cohort = line.chomp.split(",")
     @students << {name: name, cohort: cohort.to_sym}
   end
   file.close
 end
+
+def try_load_students
+  filename = ARGV.first #first argument from the commmand line
+  return if filename.nil? #get out of the method if it isn't given
+  if File.exists?(filename) #does it exist?
+    load_students(filename)
+    puts "Loaded #{@students.count} from #{filename}"
+  else #if it doesn't exist
+    puts "Sorry, #{filename} doesn't exist."
+    exit #quits the program
+  end
+end
+
+try_load_students
 interactive_menu
